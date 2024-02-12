2ab00c5c8b184698f716c3a9644014593c69f257
shakg <1sh4kg0nu1@gmail.com>
2024-02-12 22:16:25 +0300

    chore: remove login endpoint, default route will be homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from "@angular/router";
 import { HomepageComponent } from "./pages/homepage/homepage.component";
-import { LoginComponent } from "./pages/login/login.component";
 import { ResultComponent } from "./pages/result/result.component";
 
 export const routes: Routes = [
-  { path: "login", component: LoginComponent },
+//  { path: "login", component: LoginComponent },
   { path: "homepage", component: HomepageComponent },
   { path: "result", component: ResultComponent },
-  { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: "", redirectTo: "/homepage", pathMatch: "full" },
 ];
